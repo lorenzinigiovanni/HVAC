9b37b23aa13562567bb0d9344d713db76e4b6152
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-12-13 21:23:57 +0100

    Add Aircon class

---
+++ Software/Controller/src/actuators/aircon.ts
@@ -0,0 +1,169 @@
+import mqtt from 'mqtt';
+
+export class Aircon {
+
+    private _on: boolean = false;
+    get on(): boolean {
+        return this._on;
+    }
+    set on(value: boolean) {
+        this._on = value;
+        if (this._on)
+            this._client.publish(this._topic + '/power', '1');
+        else
+            this._client.publish(this._topic + '/power', '0');
+    }
+
+    private _mode: Aircon.Mode = Aircon.Mode.Ventilation;
+    get mode(): Aircon.Mode {
+        return this._mode;
+    }
+    set mode(value: Aircon.Mode) {
+        this._mode = value;
+        this._client.publish(this._topic + '/mode', this._mode);
+    }
+
+    private _temperature: number = 0;
+    get temperature(): number {
+        return this._temperature;
+    }
+    set temperature(value: number) {
+        this._temperature = value;
+        this._client.publish(this._topic + '/temp', this._temperature.toString());
+    }
+
+    private _fanSpeed: Aircon.FanSpeed = Aircon.FanSpeed.Low;
+    get fanSpeed(): Aircon.FanSpeed {
+        return this._fanSpeed;
+    }
+    set fanSpeed(value: Aircon.FanSpeed) {
+        this._fanSpeed = value;
+        this._client.publish(this._topic + '/fan', this._fanSpeed);
+    }
+
+    private _vSwing: Aircon.VerticalSwing = Aircon.VerticalSwing.Auto;
+    get vSwing(): Aircon.VerticalSwing {
+        return this._vSwing;
+    }
+    set vSwing(value: Aircon.VerticalSwing) {
+        this._vSwing = value;
+        this._client.publish(this._topic + '/vswing', this._vSwing);
+    }
+
+    private _hSwing: Aircon.HorizontalSwing = Aircon.HorizontalSwing.Step3;
+    get hSwing(): Aircon.HorizontalSwing {
+        return this._hSwing;
+    }
+    set hSwing(value: Aircon.HorizontalSwing) {
+        this._hSwing = value;
+        this._client.publish(this._topic + '/hswing', this._hSwing);
+    }
+
+    private _energyControl: Aircon.EnergyControl = Aircon.EnergyControl.Off;
+    get energyControl(): Aircon.EnergyControl {
+        return this._energyControl;
+    }
+    set energyControl(value: Aircon.EnergyControl) {
+        this._energyControl = value;
+        this._client.publish(this._topic + '/energy', this._energyControl);
+    }
+
+    private _autoClean: boolean = false;
+    get autoClean(): boolean {
+        return this._autoClean;
+    }
+    set autoClean(value: boolean) {
+        this._autoClean = value;
+        if (this._autoClean)
+            this._client.publish(this._topic + '/autoclean', '1');
+        else
+            this._client.publish(this._topic + '/autoclean', '0');
+    }
+
+    private _silent: boolean = false;
+    get silent(): boolean {
+        return this._silent;
+    }
+    set silent(value: boolean) {
+        this._silent = value;
+        if (this._silent)
+            this._client.publish(this._topic + '/silent', '1');
+        else
+            this._client.publish(this._topic + '/silent', '0');
+    }
+
+    private _client: mqtt.MqttClient;
+    private _topic: string;
+
+    constructor(client: mqtt.MqttClient, topic: string) {
+        this._client = client;
+        this._topic = topic;
+    }
+
+    showKW() {
+        this._client.publish(this._topic + '/showkw', '');
+    }
+
+    jet() {
+        this._client.publish(this._topic + '/jet', '');
+    }
+
+    light() {
+        this._client.publish(this._topic + '/light', '');
+    }
+
+    send() {
+        this._client.publish(this._topic + '/send', '');
+    }
+
+}
+
+export namespace Aircon {
+
+    export enum Mode {
+        Cooling = 'cool',
+        Dehumidification = 'dehum',
+        Ventilation = 'vent',
+        Heating = 'heat'
+    }
+
+    export enum FanSpeed {
+        Low = 'low',
+        MidLow = 'midlow',
+        Mid = 'mid',
+        MidHigh = 'midhigh',
+        High = 'high',
+        NaturalWind = 'naturalwind'
+    }
+
+    export enum VerticalSwing {
+        Auto = 'auto',
+        Step1 = 'step1',
+        Step2 = 'step2',
+        Step3 = 'step3',
+        Step4 = 'step4',
+        Step5 = 'step5',
+        Step6 = 'step6',
+        Swing = 'swing'
+    };
+
+    export enum HorizontalSwing {
+        Auto = 'auto',
+        Step1 = 'step1',
+        Step2 = 'step2',
+        Step3 = 'step3',
+        Step4 = 'step4',
+        Step5 = 'step5',
+        Swing = 'swing',
+        SwingLeft = 'swingleft',
+        SwingRight = 'swingright'
+    };
+
+    export enum EnergyControl {
+        Off = '0',
+        Minus20 = '20',
+        Minus40 = '40',
+        Minus60 = '60'
+    };
+
+}
--- Software/Controller/src/index.ts
+++ Software/Controller/src/index.ts
@@ -1,6 +1,8 @@
 import { AmbientTemperatureController } from "./controllers/ambientTemperatureController";
 import { Room } from "./controllers/room";
 import { Attutatore, TipoAttuatore } from "./controllers/attuatore";
+import { Aircon } from "./actuators/aircon";
+import mqtt from 'mqtt';
 
 let currentTemperature: number = 0;
 let currentHumidity: number = 0;
@@ -14,25 +16,20 @@ atc1.kD = 0.1;
 atc1.currentTemperature = 20;
 atc1.targetTemperature = 21;*/
 
-const mqtt = require('mqtt')
-
 let client = mqtt.connect('mqtt://192.168.69.220')
 
 client.on('connect', function () {
-    client.subscribe('ciro/munnezz/+', function (err: any) {
-        if (!err) {
-            client.publish('ciro/munnezz/a', 'Hello mqtt');
-        }
-    });
+    client.subscribe('sonda/+');
+    client.subscribe('ac/+');
 });
 
 client.on('message', function (topic: string, message: Buffer) {
     console.log(topic);
     console.log(message.toString());
 
-    if (topic == 'ciro/munnezz/temperature')
+    if (topic == 'sonda/temperature')
         currentTemperature = +message.toString();
-    if (topic == 'ciro/munnezz/humidity')
+    if (topic == 'sonda/humidity')
         currentHumidity = +message.toString();
 
     accessory
@@ -95,3 +92,88 @@ accessory
     });
 
 bridge.addBridgedAccessory(accessory);
+
+let aircon = new Aircon(client, 'ac');
+
+const ac = new Accessory('AC LG', uuid.generate('AC LG'));
+ac.on(AccessoryEventTypes.IDENTIFY, (paired: boolean, callback: any) => {
+    ac.identify();
+    callback();
+});
+
+let fanService = ac.addService(Service.Fan, 'Blower')
+
+fanService.getCharacteristic(Characteristic.On)!
+    .on(CharacteristicEventTypes.SET, (value: any, callback: any) => {
+        console.log("Fan power changed to " + value);
+
+        aircon.on = value;
+        aircon.send();
+
+        callback(null, currentTemperature);
+    });
+
+fanService.addCharacteristic(Characteristic.RotationSpeed)
+    .on(CharacteristicEventTypes.SET, (value: any, callback: any) => {
+        console.log("Setting fan rSpeed to %s", value);
+
+        if (value <= 20)
+            aircon.fanSpeed = Aircon.FanSpeed.Low;
+        else if (value <= 40)
+            aircon.fanSpeed = Aircon.FanSpeed.MidLow;
+        else if (value <= 60)
+            aircon.fanSpeed = Aircon.FanSpeed.Mid;
+        else if (value <= 80)
+            aircon.fanSpeed = Aircon.FanSpeed.MidHigh;
+        else
+            aircon.fanSpeed = Aircon.FanSpeed.High;
+
+        aircon.send();
+
+        callback();
+    });
+
+fanService.addCharacteristic(Characteristic.SwingMode)
+    .on(CharacteristicEventTypes.SET, (value: any, callback: any) => {
+        console.log("Setting horizontal swing to %s", value);
+
+        if (value)
+            aircon.hSwing = Aircon.HorizontalSwing.Swing;
+        else
+            aircon.hSwing = Aircon.HorizontalSwing.Step3;
+
+        callback();
+    });
+
+var thermostatService = ac.addService(Service.Thermostat, "Thermostat");
+thermostatService.addLinkedService(fanService);
+
+thermostatService.getCharacteristic(Characteristic.TargetHeatingCoolingState)!
+    .on(CharacteristicEventTypes.SET, (value: any, callback: any) => {
+        console.log("Characteristic TargetHeatingCoolingState changed to %s", value);
+
+        if (value == 0)
+            aircon.mode = Aircon.Mode.Ventilation;
+        else if (value == 1)
+            aircon.mode = Aircon.Mode.Heating;
+        else if (value == 2)
+            aircon.mode = Aircon.Mode.Cooling;
+        else if (value == 3)
+            aircon.mode = Aircon.Mode.Dehumidification;
+
+        aircon.send();
+
+        callback();
+    });
+
+thermostatService.getCharacteristic(Characteristic.TargetTemperature)!
+    .on(CharacteristicEventTypes.SET, (value: any, callback: any) => {
+        console.log("Characteristic TargetTemperature changed to %s", value);
+
+        aircon.temperature = value;
+        aircon.send();
+
+        callback();
+    });
+
+bridge.addBridgedAccessory(ac);
