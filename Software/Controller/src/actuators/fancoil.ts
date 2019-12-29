abad6cea1a4a9376a9e25fda9b8898a417444806
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-12-29 11:27:51 +0100

    Create fancoil.ts

---
+++ Software/Controller/src/actuators/fancoil.ts
@@ -0,0 +1,26 @@
+import mqtt from 'mqtt';
+
+export class Fancoil {
+
+    private _on: boolean = false;
+    get on(): boolean {
+        return this._on;
+    }
+    set on(value: boolean) {
+        this._on = value;
+        if (this.on)
+            this._client.publish(this._topic + '/on', '1');
+        else
+            this._client.publish(this._topic + '/on', '0');
+    }
+
+    private _client: mqtt.MqttClient;
+    private _topic: string;
+
+    constructor(client: mqtt.MqttClient, topic: string) {
+        this._client = client;
+        this._topic = topic + '/fancoil';
+    }
+
+}
+
