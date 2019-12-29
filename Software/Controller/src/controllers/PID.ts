a32a8c503e9ef5211ecf76508a5b6873215aaa5c
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-12-29 11:29:19 +0100

    Update PID.ts

--- Software/Controller/src/controllers/PID.ts
+++ Software/Controller/src/controllers/PID.ts
@@ -1,45 +1,44 @@
 export class PID {
-    private iError: number = 0;
-    private lastError: number = 0;
-    private lastTime: number = 0;
-    private currentValue: number = 0;
+    private _iError: number = 0;
+    private _lastError: number = 0;
+    private _lastTime: number = 0;
 
     kP: number = 0;
     kI: number = 0;
     kD: number = 0;
     maxI: number = Number.MAX_VALUE;
     target: number = 0;
+    currentValue: number = 0;
 
-    getControl(currentValue: number): number {
+    update(): number {
         let dt;
         let currentTime = Date.now();
-        if (this.lastTime === 0) {
+        if (this._lastTime === 0) {
             dt = 0;
         } else {
-            dt = (currentTime - this.lastTime) / 1000;
+            dt = (currentTime - this._lastTime) / 1000;
         }
-        this.lastTime = currentTime;
+        this._lastTime = currentTime;
 
-        this.currentValue = currentValue;
         let error = (this.target - this.currentValue);
 
-        this.iError = this.iError + error * dt;
-        if (Math.abs(this.iError) > this.maxI) {
-            this.iError = (this.iError > 0) ? this.maxI : -this.maxI;
+        this._iError = this._iError + error * dt;
+        if (Math.abs(this._iError) > this.maxI) {
+            this._iError = (this._iError > 0) ? this.maxI : -this.maxI;
         }
 
         let dError = 0;
         if (dt > 0) {
-            dError = (error - this.lastError) / dt;
-            this.lastError = error;
+            dError = (error - this._lastError) / dt;
+            this._lastError = error;
         }
 
-        return (this.kP * error) + (this.kI * this.iError) + (this.kD * dError);
+        return (this.kP * error) + (this.kI * this._iError) + (this.kD * dError);
     }
 
     reset() {
-        this.iError = 0;
-        this.lastError = 0;
-        this.lastTime = 0;
+        this._iError = 0;
+        this._lastError = 0;
+        this._lastTime = 0;
     }
 }
