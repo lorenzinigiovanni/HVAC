3699fada745b78cd9a6bcd0f6da03250702ac5a8
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-12-30 21:12:03 +0100

    Add events for AmbientSensor

--- Software/Controller/src/sensors/ambientSensor.ts
+++ Software/Controller/src/sensors/ambientSensor.ts
@@ -1,4 +1,6 @@
-export class AmbientSensor {
+import { EventEmitter } from "events";
+
+export class AmbientSensor extends EventEmitter {
 
     private _temperature: number = 0.0;
     get temperature(): number {
@@ -13,17 +15,28 @@ export class AmbientSensor {
     private _topic: string;
 
     constructor(topic: string) {
+        super();
+
         this._topic = topic + '/ambientsensor';
 
         global.mqttClient.subscribe(this._topic + '/temperature');
         global.mqttClient.subscribe(this._topic + '/humidity');
 
         global.mqttClient.on('message', (topic: string, message: Buffer) => {
-            if (topic == this._topic + '/temperature')
+            if (topic == this._topic + '/temperature') {
                 this._temperature = +message.toString();
-            else if (topic == this._topic + '/humidity')
+                this.emit('newTemperature', this._temperature);
+            }
+            else if (topic == this._topic + '/humidity') {
                 this._humidity = +message.toString();
+                this.emit('newHumidity', this._humidity);
+            }
         });
     }
 
+}
+
+export interface AmbientSensor {
+    on(event: 'newTemperature', listener: (temperature: number) => void): this;
+    on(event: 'newHumidity', listener: (humidity: number) => void): this;
 }
