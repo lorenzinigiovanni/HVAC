fcef6a5d497f3322e6229e7463b00a62d6cb8b0b
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-12-30 21:13:05 +0100

    Use AmbientSensor events in Outdoors

--- Software/Controller/src/places/outdoors.ts
+++ Software/Controller/src/places/outdoors.ts
@@ -18,9 +18,19 @@ export class Outdoors {
         this._topic = 'outdoors/' + this._name.toLocaleLowerCase().replace(/\s+/g, '');
 
         this._ambientSensor = new AmbientSensor(this._topic);
+        this._ambientSensor.on('newTemperature', this._onNewTemperature);
+        this._ambientSensor.on('newHumidity', this._onNewHumidity);
 
         this._homekitHumiditySensor = new HomekitHumiditySensor(this._name);
         this._homekitTemperatureSensor = new HomekitTemperatureSensor(this._name);
+    }
+
+    private _onNewTemperature = (temperature: number) => {
+        this._homekitTemperatureSensor.temperature = temperature;
+    }
+
+    private _onNewHumidity = (humidity: number) => {
+        this._homekitHumiditySensor.humidity = humidity;
     }
 
 }
