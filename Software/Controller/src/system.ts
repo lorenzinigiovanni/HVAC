8bbc620bb2be33a0ddf66bce6284401d529abd7a
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2019-12-30 21:14:08 +0100

    Aggiornati nomi dispositivi

--- Software/Controller/src/system.ts
+++ Software/Controller/src/system.ts
@@ -19,24 +19,24 @@ export class System {
         this._home.push(new Room('Cucina', true, true, false, false));
         this._home.push(new Room('Salotto', true, true, true, false));
         this._home.push(new Room('Corridoio', true, false, false, false));
-        this._home.push(new Room('Bagno1', true, false, false, false));
-        this._home.push(new Room('Bagno2', true, false, false, false));
-        this._home.push(new Room('Camera1', true, false, false, false));
-        this._home.push(new Room('Camera2', true, true, false, false));
-        this._home.push(new Room('Camera3', true, false, false, false));
+        this._home.push(new Room('Bagno 1', true, false, false, false));
+        this._home.push(new Room('Bagno 2', true, false, false, false));
+        this._home.push(new Room('Camera 1', true, false, false, false));
+        this._home.push(new Room('Camera 2', true, true, false, false));
+        this._home.push(new Room('Camera 3', true, false, false, false));
         this._home.push(new Room('Giroscale', true, false, false, false));
-        this._home.push(new Room('UfficioTorretta', true, true, false, false));
+        this._home.push(new Room('Ufficio Torretta', true, true, false, false));
 
         this._workshop = new Room('Falegnameria', true, false, false, false);
-        this._office = new Room('UfficioFalegnameria', false, false, false, true);
-        this._wc = new Room('BagnoFalegnameria', false, false, false, true);
+        this._office = new Room('Ufficio Falegnameria', false, false, false, true);
+        this._wc = new Room('Bagno Falegnameria', false, false, false, true);
 
-        this._outdoors = new Outdoors('Fuori');
+        this._outdoors = new Outdoors('Esterno');
 
         this._pumps.push(new Pump('Casa'));
         this._pumps.push(new Pump('Falegnameria'));
-        this._pumps.push(new Pump('UfficioFalegnameria'));
-        this._pumps.push(new Pump('BagnoFalegnameria'));
+        this._pumps.push(new Pump('Ufficio Falegnameria'));
+        this._pumps.push(new Pump('Bagno Falegnameria'));
 
         this._mixValves.push(new MixValve('Casa'));
         this._mixValves.push(new MixValve('Falegnameria'));
