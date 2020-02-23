2547fa78c2b39233a09f5e1a291c3094989b0a38
Giovanni Lorenzini <giovanni@lorenzinigiovanni.com>
2020-02-23 20:32:36 +0100

    Remove air conditioner code

--- Software/CiroSonda/src/main.cpp
+++ Software/CiroSonda/src/main.cpp
@@ -1,20 +1,11 @@
 #include <Arduino.h>
 #include <Adafruit_BME280.h>
 #include <WiFi.h>
-#include <ArduinoJson.h>
-#include <LGAircon.h>
-#include "esp32-hal-cpu.h"
-
-extern "C"
-{
-#include "freertos/FreeRTOS.h"
-#include "freertos/timers.h"
-}
 
 #include <AsyncMqttClient.h>
 
 #define uS_TO_S_FACTOR 1000000
-#define TIME_TO_SLEEP 10
+#define TIME_TO_SLEEP 60
 
 #define WIFI_SSID "Lorenzini"
 #define WIFI_PASSWORD "2444666668888888"
@@ -22,8 +13,7 @@ extern "C"
 #define MQTT_HOST IPAddress(192, 168, 69, 220)
 #define MQTT_PORT 1883
 
-#define baseTopic "cameragiovanni"
-#define AC
+#define baseTopic "camera2"
 
 AsyncMqttClient mqttClient;
 TimerHandle_t mqttReconnectTimer;
@@ -31,10 +21,6 @@ TimerHandle_t wifiReconnectTimer;
 TimerHandle_t sensorReadTimer;
 
 Adafruit_BME280 bme;
-
-#ifdef AC
-LGAircon ac(25, false);
-#endif
 
 void readSensor();
 void connectToWifi();
@@ -46,19 +32,21 @@ void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties 
 
 void setup()
 {
-  setCpuFrequencyMhz(80);
-
   Serial.begin(9600);
 
-  bme.begin(&Wire);
+  if (!bme.begin(&Wire))
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    sleep();
+  }
 
-#ifdef AC
-  ac.begin();
-#endif
+  bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                  Adafruit_BME280::SAMPLING_X1,   // temperature
+                  Adafruit_BME280::SAMPLING_NONE, // pressure
+                  Adafruit_BME280::SAMPLING_X1,   // humidity
+                  Adafruit_BME280::FILTER_OFF);
 
-  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(10000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
-  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(10000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
-  sensorReadTimer = xTimerCreate("sensorTimer", pdMS_TO_TICKS(60000), pdFALSE, (void *)0, reinterpret_cast<TimerCallbackFunction_t>(readSensor));
+  bme.takeForcedMeasurement();
 
   WiFi.onEvent(WiFiEvent);
 
@@ -67,19 +55,19 @@ void setup()
   mqttClient.onMessage(onMqttMessage);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
 
-  connectToWifi();
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void loop()
 {
-  delay(1000);
 }
 
-/*void sleep()
+void sleep()
 {
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   esp_deep_sleep_start();
-}*/
+}
 
 void readSensor()
 {
@@ -91,27 +79,13 @@ void readSensor()
 
   snprintf(topic, 50, "room/%s/ambientsensor/temperature", baseTopic);
   dtostrf(t, 6, 2, result);
-  mqttClient.publish(topic, 0, true, result);
+  mqttClient.publish(topic, 0, false, result);
 
   snprintf(topic, 50, "room/%s/ambientsensor/humidity", baseTopic);
   dtostrf(h, 6, 2, result);
-  mqttClient.publish(topic, 0, true, result);
+  mqttClient.publish(topic, 0, false, result);
 
-  xTimerStart(sensorReadTimer, 0);
-
-  // sleep();
-}
-
-void connectToWifi()
-{
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-}
-
-void connectToMqtt()
-{
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
+  sleep();
 }
 
 void WiFiEvent(WiFiEvent_t event)
@@ -121,14 +95,17 @@ void WiFiEvent(WiFiEvent_t event)
   {
   case SYSTEM_EVENT_STA_GOT_IP:
     Serial.println("WiFi connected");
+
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-    connectToMqtt();
+
+    Serial.println("Connecting to MQTT...");
+    mqttClient.connect();
     break;
+
   case SYSTEM_EVENT_STA_DISCONNECTED:
     Serial.println("WiFi lost connection");
-    xTimerStop(mqttReconnectTimer, 0);
-    xTimerStart(wifiReconnectTimer, 0);
+    sleep();
     break;
   }
 }
@@ -136,23 +113,13 @@ void WiFiEvent(WiFiEvent_t event)
 void onMqttConnect(bool sessionPresent)
 {
   Serial.println("MQTT connected");
-  //readSensor();
-  xTimerStart(sensorReadTimer, 0);
-#ifdef AC
-  char topic[50];
-  snprintf(topic, 50, "room/%s/ac/+", baseTopic);
-  mqttClient.subscribe(topic, 0);
-#endif
+  readSensor();
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason)
 {
   Serial.println("Disconnected from MQTT.");
-
-  if (WiFi.isConnected())
-  {
-    xTimerStart(mqttReconnectTimer, 0);
-  }
+  sleep();
 }
 
 void onMqttMessage(char *topic, char *payloadO, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
@@ -165,108 +132,4 @@ void onMqttMessage(char *topic, char *payloadO, AsyncMqttClientMessageProperties
   Serial.print(topic);
   Serial.print(" Payload: ");
   Serial.println(payload);
-
-#ifdef AC
-  if (strstr(topic, "ac/set") != NULL)
-  {
-    StaticJsonDocument<200> doc;
-    DeserializationError error = deserializeJson(doc, payload);
-
-    if (error)
-    {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.c_str());
-      return;
-    }
-
-    const char *mode = doc["mode"];
-    uint8_t temperature = doc["temperature"];
-    const char *fanSpeed = doc["fanSpeed"];
-
-    ac.set(mode, temperature, fanSpeed);
-  }
-  else if (strstr(topic, "ac/off") != NULL)
-  {
-    ac.turnOff();
-  }
-  else if (strstr(topic, "ac/vswing") != NULL)
-  {
-    if (strstr(payload, "auto") != NULL)
-      ac.setVSwing(VerticalSwing::Auto);
-    else if (strstr(payload, "step1") != NULL)
-      ac.setVSwing(VerticalSwing::Step1);
-    else if (strstr(payload, "step2") != NULL)
-      ac.setVSwing(VerticalSwing::Step2);
-    else if (strstr(payload, "step3") != NULL)
-      ac.setVSwing(VerticalSwing::Step3);
-    else if (strstr(payload, "step4") != NULL)
-      ac.setVSwing(VerticalSwing::Step4);
-    else if (strstr(payload, "step5") != NULL)
-      ac.setVSwing(VerticalSwing::Step5);
-    else if (strstr(payload, "step6") != NULL)
-      ac.setVSwing(VerticalSwing::Step6);
-    else if (strstr(payload, "swing") != NULL)
-      ac.setVSwing(VerticalSwing::Swing);
-  }
-  else if (strstr(topic, "ac/hswing") != NULL)
-  {
-    if (strstr(payload, "auto") != NULL)
-      ac.setHSwing(HorizontalSwing::Auto);
-    else if (strstr(payload, "step1") != NULL)
-      ac.setHSwing(HorizontalSwing::Step1);
-    else if (strstr(payload, "step2") != NULL)
-      ac.setHSwing(HorizontalSwing::Step2);
-    else if (strstr(payload, "step3") != NULL)
-      ac.setHSwing(HorizontalSwing::Step3);
-    else if (strstr(payload, "step4") != NULL)
-      ac.setHSwing(HorizontalSwing::Step4);
-    else if (strstr(payload, "step5") != NULL)
-      ac.setHSwing(HorizontalSwing::Step5);
-    else if (strstr(payload, "swing") != NULL)
-      ac.setHSwing(HorizontalSwing::Swing);
-    else if (strstr(payload, "swingleft") != NULL)
-      ac.setHSwing(HorizontalSwing::SwingLeft);
-    else if (strstr(payload, "swingright") != NULL)
-      ac.setHSwing(HorizontalSwing::SwingRight);
-  }
-  else if (strstr(topic, "ac/autoclean") != NULL)
-  {
-    if (strstr(payload, "1") != NULL)
-      ac.setAutoClean(true);
-    else if (strstr(payload, "0") != NULL)
-      ac.setAutoClean(false);
-  }
-  else if (strstr(topic, "ac/silent") != NULL)
-  {
-    if (strstr(payload, "1") != NULL)
-      ac.setSilent(true);
-    else if (strstr(payload, "0") != NULL)
-      ac.setSilent(false);
-  }
-  else if (strstr(topic, "ac/energy") != NULL)
-  {
-    if (strstr(payload, "0") != NULL)
-      ac.setEnergyControl(EnergyControl::Off);
-    else if (strstr(payload, "20") != NULL)
-      ac.setEnergyControl(EnergyControl::Minus20);
-    else if (strstr(payload, "40") != NULL)
-      ac.setEnergyControl(EnergyControl::Minus40);
-    else if (strstr(payload, "60") != NULL)
-      ac.setEnergyControl(EnergyControl::Minus60);
-  }
-  else if (strstr(topic, "ac/showkw") != NULL)
-  {
-    ac.showKW();
-  }
-  else if (strstr(topic, "ac/jet") != NULL)
-  {
-    ac.jet();
-  }
-  else if (strstr(topic, "ac/light") != NULL)
-  {
-    ac.light();
-  }
-
-  ac.print();
-#endif
 }
